8da061ba0f48c04fbabcda904bae64fe8e5a07b0
Rafla Dzierzbinski 180157 <dzemba@op.pl>
2015-05-01 18:31:26 +0200

    Zaimplementowanei testu klasy BookKeeper

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,14 +1,47 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.Assert.*;
+import java.util.Date;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 
 public class BookKeeperTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	private BookKeeper bookKeeper;
+	
+		@Test
+		public void requestInvoiceWithOnePosition_shouldReturnInvoiceWithOnePosition() {
+	
+			// given
+			Id id = new Id("999");
+			Money money = new Money(1);
+			InvoiceFactory mockInvoiceFactory = mock(InvoiceFactory.class);
+			bookKeeper = new BookKeeper(mockInvoiceFactory);
+			ClientData clientData = new ClientData(id, "Test");
+			when(mockInvoiceFactory.create(clientData)).thenReturn(new Invoice(id, clientData));
+			
+			InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+			TaxPolicy taxPolicy = mock(TaxPolicy.class);
+						
+			ProductData productData = new ProductData(id, money, "ksiazka",ProductType.FOOD, new Date());
+			
+			when(taxPolicy.calculateTax(ProductType.FOOD, money)).thenReturn(new Tax(money, "spis"));
+			
+			RequestItem requestItem = new RequestItem(productData, 4, money);invoiceRequest.add(requestItem);
+
+			assertThat(bookKeeper.issuance(invoiceRequest, taxPolicy).getItems().size(), is(1));
+		}
 
 }
